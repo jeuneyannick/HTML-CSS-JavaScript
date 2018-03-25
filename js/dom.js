5c7024980a2ea0fc5497144f1706e19f29d4f141
Yannick Bley <yannick.bley@lepoles.com>
2018-03-25 09:49:26 +0200

    commit

---
+++ js/dom.js
@@ -0,0 +1,40 @@
+
+var h = document.head; //la variable h correspond l'objet head du DOM 
+console.log(h); 
+var b = document.body;// la variable b correspond à l'objet body du DOM
+console.log(b); 
+
+if(document.body.nodeType === document.ELEMENT_NODE){
+   console.log("Body est un noeud élément"); 
+} else{
+    console.log("Body est un noeud textuel"); 
+}; 
+//Il y a deux types de noeud dans l'arborescence d'objets du DOM
+// les ELEMENT_NODE qui sont des noeuds éléments qui peuvent avoir des enfants ou des sous-enfants
+// les TEXT_NODE qui sont des noeuds textuels. 
+//la propriété nodeType permet d'indiquer le type de noeud d'un objet du DOM. 
+
+//Pour accéder au premier enfant du noeud body avec la propriété childNodes
+ console.log(document.body.childNodes[0]); 
+
+ //Si cela ne renvoie rien c'est parce que les espaces entre les balises et les auts à la ligne dans le code HTML 
+ //sont considérés par le navigateur comme des noeuds textuels. 
+
+ console.log(document.body.childNodes[1]); 
+
+
+ //Afficher les noeuds enfants du body 
+
+ for ( var i = 0; i < document.body.childNodes.length;i++){
+     console.log(document.body.childNodes[i]); 
+ }; 
+
+ // Accéder au parent d'un noeud 
+
+ var h1 = document.body.childNodes[1]; 
+ console.log(h1.parentNode);// affiche l'objet body
+ console.log(document.parentNode); //affiche Null, document est à la racine du DOM il n'a logiquement pas de parent
+
+
+
+ // Exercice
