f31a4c29d7ae3dbe7ff27de67d0a324d19449d23
Yannick Bley <yannick.bley@lepoles.com>
2018-04-19 10:27:20 +0200

    commit séparation

--- js/main.js
+++ js/main.js
@@ -1,47 +1,4 @@
-//Les différents types de variables
 
-
-// Les booléens(boolean)
-console.log(true);
-console.log(false); 
-// Les nombres (NUMBER)
-console.log(54);
-console.log(6.7); 
-// Les chaines de caractères (STRING)
-console.log("je réapprends les bases de JavaScript")
-//Undefined
-//C'est une variable déclarée qui n'a pas encore de valeur définie
-console.log(undefined); 
-//Null 
-console.log(null)
-//Les objets 
-console.log({name:" Yannick "}); 
-
-
-var a 
-
-console.log(a); 
-
-/* Hoisting des fonctions 
-Bien que fonctionnant de manière séquentielle, JavaScript recherche d'abord les déclarations de fonctions et
-les fait monter dans le code (il les hisse d'où le nom de hoisting en anglais)
-*/
-addition(5,7); 
-
-function addition(a,b){
-    console.log(a + b); 
-}
-/*
-Le hoisting de fonctions n'est valable que pour les déclarations de fonction et non pour les expressions de fonctions.
-*/
-// addition2(a,b); 
-// var addition2 = function(a,b){
-//     console.log(a + b); 
-// }
-
-
-console.log(x); 
-var x = 5;
 
 
 var perso = {
